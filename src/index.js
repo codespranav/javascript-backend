3bd5d3bf904da93a7e445568db229421e9e9065f
Pranav <pranavk24h@gmail.com>
2024-03-24 09:09:42 +0530

    connection to db

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-ÿþ
+import express from "express"
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+dotenv.config({
+    path: './env'
+});
+
+const app = express();
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (async () =>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+//         app.on("error", (error)=>{
+//             console.log(error);
+//             throw error;
+//         })
+//     } catch (error) {
+//         console.error("error occurred: ", error);
+//         throw error
+//     }
+// })
